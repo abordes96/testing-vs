620c740a77be573deeb173c0b86b30aa37e2899f
abordes96 <abordesgimenez@gmail.com>
2017-05-14 20:52:00 +0200

    Pruebas generadas con IntelliTest realizadas

--- Calculadora/Calculadora/Calculadora.cs
+++ Calculadora/Calculadora/Calculadora.cs
@@ -8,6 +8,9 @@ namespace CalculadoraNS
 {
     public class Calculadora
     {
+       
+       
+
         public double Sumar(double num1, double num2)
         {
             return num1 + num2;
@@ -29,6 +32,28 @@ namespace CalculadoraNS
             if (num1 < num2) return num1;
 
             return num2;
+        }
+
+        public static String tipoTriangulo(int a, int b, int c)
+        {
+            String tipo;
+
+            if(a <= 0 || b <= 0 || c <= 0)
+            {
+             tipo = "Triangulo no valido";
+            }
+            else if (a == b && b == c)
+            {
+                tipo = "Equilatero";
+            }
+            else if (b == c || a == b || c == a)
+            {
+                tipo = "Isosceles";
+            }
+            else {
+                tipo = "Escaleno";
+            }
+            return tipo;
         }
 
     }
