aa4e215adcc7d761bd7f54ad588ac0e8fa87bd3d
abordes96 <abordesgimenez@gmail.com>
2017-05-14 14:06:26 +0200

    Implementado operaciones de suma, resta, max y min, y realizado test básico de suma y resta

--- Calculadora/Calculadora/Calculadora.cs
+++ Calculadora/Calculadora/Calculadora.cs
@@ -8,5 +8,28 @@ namespace CalculadoraNS
 {
     public class Calculadora
     {
+        public double Sumar(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        public double restar(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double max(double num1, double num2)
+        {
+            if (num1 > num2) return num1;
+            
+            return num2;
+        }
+
+        public double min(double num1, double num2)
+        {
+            if (num1 < num2) return num1;
+
+            return num2;
+        }
+
     }
 }
---
+++ Calculadora/CalculadoraTests/OperacionesElementalesTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using CalculadoraNS;
+
+namespace CalculadoraNS.Tests
+{
+    [TestClass()]
+    public class OperacionesElementalesTest
+    {
+        [TestMethod()]
+        public void SumarTest()
+        {
+            var calculadora = new Calculadora();
+            var resultado = calculadora.Sumar(1, 2);
+            Assert.AreEqual(3, resultado);
+        }
+
+        [TestMethod()]
+        public void restarTest()
+        {
+            var calculadora = new Calculadora();
+            var resultado = calculadora.restar(1, 2);
+            Assert.AreEqual(-1, resultado);
+        }
+    }
+}
+
+
